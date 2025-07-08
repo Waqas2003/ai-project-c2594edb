52a053f388137255418925d263e1270c6df38ef4
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 18:17:51 +0500

    Add src\components\Header.js

---
+++ src\components\Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Team</a></li>
+          <li><a href="#">News</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
