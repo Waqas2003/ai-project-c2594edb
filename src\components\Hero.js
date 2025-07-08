79f7cbb4c6955a18aa92b789404887c81ffaa18a
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 18:17:54 +0500

    Add src\components\Hero.js

---
+++ src\components\Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Argentina Football Team</h1>
+      <p>La Albiceleste</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Argentina_national_football_team_logo.svg/1200px-Argentina_national_football_team_logo.svg.png" alt="Argentina Football Team Logo" />
+    </section>
+  );
+}
+
+export default Hero;
