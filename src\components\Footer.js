1d8a14c8a6d731b60fcf2c2725ea97b59fb6997f
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 18:17:47 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 Argentina Football Team</p>
+    </footer>
+  );
+}
+
+export default Footer;
