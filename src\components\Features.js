1e85db87290cbd73dc6c7a436fc7a33aab13824c
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 18:17:43 +0500

    Add src\components\Features.js

---
+++ src\components\Features.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Features.css';
+
+function Features() {
+  return (
+    <section className="features">
+      <h2>Our Achievements</h2>
+      <ul>
+        <li>
+          <h3>World Cup Winners (1978, 1986)</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </li>
+        <li>
+          <h3>Copa America Winners (1991, 1993)</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Features;
